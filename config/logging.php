0e44eb22410e1678545aadb0bd0d2f11f73f0802
saminthaa <thapasamin4@gmail.com>
2025-07-03 17:16:41 +0545

    Papertrail

--- config/logging.php
+++ config/logging.php
@@ -84,14 +84,7 @@ return [
 
         'papertrail' => [
             'driver' => 'monolog',
-            'level' => env('LOG_LEVEL', 'debug'),
-            'handler' => env('LOG_PAPERTRAIL_HANDLER', SyslogUdpHandler::class),
-            'handler_with' => [
-                'host' => env('PAPERTRAIL_URL'),
-                'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
-            ],
-            'processors' => [PsrLogMessageProcessor::class],
+            'handler' => null,
         ],
 
         'stderr' => [
